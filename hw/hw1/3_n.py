282337c3433cd088d6eef9da09a3bbc5ac19eba1
KymikoWka <49477119+KymikoWka@users.noreply.github.com>
2019-09-22 17:26:48 +0300

    Update2

---
+++ hw/hw1/3_n.py
@@ -0,0 +1,5 @@
+import math
+x = float(input("Введите x "))
+y = float(input("Введите y "))
+z = float(input("Введите z "))
+print(math.sqrt(x**2 + y**2 + z**2))
---
+++ hw/hw1/6_n.py
@@ -0,0 +1,6 @@
+print("Введите число ")
+x = float(input('x: '))
+if x == int(x):
+    print("Число целое")
+else:
+    print("Число не целое")
