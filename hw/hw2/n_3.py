9590ede7c9805cd95d0786555e527032936a867b
KymikoWka <49477119+KymikoWka@users.noreply.github.com>
2019-09-30 15:03:33 +0300

    Update

--- hw/hw2/n_3.py
+++ hw/hw2/n_3.py
@@ -1,11 +1,3 @@
-def skobki(r):
-    ot = r.count("(")
-    zk = r.count(")")
-    if ot > zk:
-        return "Ne hvataet" + str(ot - zk) + "zakrivaushih skobki!"
-    elif ot < zk:
-        return "Ne hvataet" + str(zk - ot) + "otkrivaushey skobki!"
-    return "Ok!"
-kek = input("Vvod: ")
-otv  = skobki(kek)
-print(otv)
+n = int(input())
+for i in range(1, n + 1):
+    print(i * "*")
--- hw/hw2/n_4.py
+++ hw/hw2/n_4.py
@@ -1,3 +1,11 @@
-s = input()
-for x in s:
-    print(x)
+def skobki(r):
+    ot = r.count("(")
+    zk = r.count(")")
+    if ot > zk:
+        return "Ne hvataet" + str(ot - zk) + "zakrivaushih skobki!"
+    elif ot < zk:
+        return "Ne hvataet" + str(zk - ot) + "otkrivaushey skobki!"
+    return "Ok!"
+kek = input("Vvod: ")
+otv  = skobki(kek)
+print(otv)
