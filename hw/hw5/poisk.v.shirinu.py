f2a9b9cff3f991f25f059f9511a8befce92c4fd6
KymikoWka <49477119+KymikoWka@users.noreply.github.com>
2019-10-29 11:04:58 +0300

    Update poisk v shirinu

--- hw/hw5/poisk.v.shirinu.py
+++ hw/hw5/poisk.v.shirinu.py
@@ -2,22 +2,23 @@ from collections import deque
 
 
 def players(s):
-    return not len(s) % 2 and s[0] == "D"
+    return not len(s) % 2 and s[0] == "D"  # len - длина
 
 
 names = {
     "Dima": ["Alice", "Paul"],
-    "ALice": ["Carl", "Denis", "Edward"],
+    "Alice": ["Carl", "Denis", "Edward"],
     "Paul": ["Denis", "Elise", "Nikita", "Alice", "Dona"],
 }
 
-d = deque(names["Dima"])
+d = deque(names["Dima"])  # Очередь
 x = []
 while names:
-    doter = d.popleft()
-    if doter not in x:
-        if players(doter):
+    doter = d.popleft()  # Удаляет и возвращает первый элемент очереди
+    if doter not in x:  # Чтоб не повторялись друзья
+        if players(doter):  # Еcли ТРУ то печатаю и выхожу из цикла
             print(doter)
-            break
+            break  # Выход из while
+        # В очередь докидываем друзей текущего человека, get - получение значения по ключу Doter
         d += names.get(doter, [])
-        x.append(doter)
+        x.append(doter)  # Добавление в список тех, кого проверили
