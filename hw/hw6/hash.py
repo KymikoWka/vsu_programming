ee8277ddb11fae832f131ddcfb7282aecebca765
KymikoWka <49477119+KymikoWka@users.noreply.github.com>
2019-11-10 19:47:01 +0300

    hash

--- hw/hw6/hash.py
+++ hw/hw6/hash.py
@@ -1,53 +1,67 @@
-storage = [[] for x in range(15)]
+# Это список из списков который будет длиной из 3 элементов
+# Он нужен для хранения ключа и его значения
+hash_table = [[] for _ in range(3)]
 
 
-def hash(key):
-    index = sum(list(map(ord, key)))
-    index = sum(list(map(int, str(index))))
-    return index % len(storage)
+# Это функция которая возвращает хеш от ключа.
+def hash_func(hash_str):
+    hash_str = sum([ord(x) for x in hash_str]) % 3
+    return hash_str
 
 
+# Это функция которая добавляет пару ключ значение в список или обновляет её.
 def set_value(key, value):
-    index = hash(key)
-    for i in storage[index]:
-        if key == i[0]:
-            i[1] = value
+    # В пременнную записываем спиcок с парами.
+    el = hash_table[hash_func(key)]
+    # Итерируемся по парам.
+    for pair in el:
+        # Проверяем совпадают ли ключи у выбранной пары.
+        if key == pair[0]:
+            # Если совпадают, то перезаписываем значение у пары.
+            pair[1] = value
             break
+    # else выполнится, если цикл вышел не через break.
     else:
-        storage[index].append([key, value])
+        # Добавляем новую пару в список.
+        el.append([key, value])
 
 
-def del_value(key):
-    index = hash(key)
-    for i in storage[index]:
-        if key == i[0]:
-            storage[index].remove(i)
-            break
-
-
+# Функция которая возвращает значение ключа, которое введёт пользователь.
 def get_value(key):
-    index = hash(key)
-    for i in storage[index]:
-        if key == i[0]:
-            return i[1]
+    # Цикл которай итерируется по парам(но мы сразу развернули значение и ключ)
+    for k, v in hash_table[hash_func(key)]:
+        # Проверяем ключи.
+        if key == k:
+            # Возвращаем значение.
+            return v
 
 
-print(storage)
+# Цикл удаления ключа.
+def del_pair(key):
+    el = hash_table[hash_func(key)]
+    for pair in el:
+        if key == pair[0]:
+            el.remove(pair)
+            return True
+    return False
 
-set_value('qwe', 1)
-set_value('qwe', 100)
 
-set_value('asd', 2)
-set_value('asd', 200)
+set_value("abc", 1)
+set_value("zxc", 2)
+set_value("fd", 3)
+set_value("a", 4)
+set_value("d", 5)
+set_value("2134", 6)
+set_value("142155", 7)
 
-set_value('zxc', 3)
-set_value('zxc', 300)
+set_value("abc", 999999999999)
+print(get_value("abc"))
+print(get_value("zxc"))
+print(get_value("fd"))
+print(get_value("a"))
+print(get_value("d"))
+print(get_value("2134"))
+print(get_value("142155"))
+print(get_value("142155e"))
 
-print(storage)
-
-del_value('zxc')
-
-print(get_value('qwe'))
-print(get_value('asd'))
-
-print(storage)
+print(del_pair("abc"))
